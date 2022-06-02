83b622c577cb2b9f0d3eb977f912a6b956afddef
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-06-02 10:46:43 +0200

    Class CustomError created

---
+++ src/main/java/Infrastructure/exception/CustomError.java
@@ -0,0 +1,14 @@
+package Infrastructure.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+// Clase con la que tendremos los atributos de nuestro error
+public class CustomError {
+    private Date timestamp;
+    private int httpCode;
+    private String message;
+}
